6235166a26e497de057df4a9f66e37bc7fccfccd
Tobias Wahyudi <tobias@medmehealth.com>
2025-02-23 00:14:12 -0500

    House

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 //! A simple 3D scene with light shining over a cube sitting on a plane.
 
 use bevy::prelude::*;
-use bevy::render::mesh::shape;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+// STRUCTS AND STUFF
+// MAIN
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Village".to_string(),
+                resolution: (1280.0, 720.0).into(),
+                resizable: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(WorldInspectorPlugin::new())
         .add_systems(Startup, setup)
         .run();
 }
@@ -18,16 +30,23 @@ fn setup(
 ) {
     // circular base
     commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Circle::new(4.0).into()),
-        material: materials.add(Color::rgb(0.2, 0.89, 0.2).into()),
+        mesh: meshes.add(Circle::new(4.0)),
+        material: materials.add(Color::srgb_u8(50, 200, 50)),
         transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
         ..default()
     });
     // cube
     commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Box::new(1.0, 1.0, 1.0).into()),
-        material: materials.add(Color::rgb(0.78, 0.89, 1.0).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        mesh: meshes.add(Cuboid::new(0.8, 0.6, 1.0)),
+        material: materials.add(Color::srgb_u8(120, 80, 10)),
+        transform: Transform::from_xyz(0.0, 0.3, 0.0),
+        ..default()
+    });
+    // Triangular prism
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Extrusion::new(Triangle2d::new(Vec2::new(-0.4, 0.0), Vec2::new(0.4, 0.0), Vec2::new(0.0, 0.3)), 1.0)),
+        material: materials.add(Color::srgb_u8(100, 70, 10)),
+        transform: Transform::from_xyz(0.0, 0.6, 0.0),
         ..default()
     });
     // light
